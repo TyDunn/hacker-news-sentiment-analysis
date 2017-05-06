fcfc2dad67848a04f95ac1f31deb98861cf31214
Ty <tydunn@umich.edu>
2017-05-06 09:53:20 -0400

    setting things up on linux server

---
+++ README.md
@@ -0,0 +1,3 @@
+# hacker-news-sentiment-analysis
+
+Please look at hacker-news-sentiment-analysis.Rmd to understand this project and its files
---
+++ hacker-news-sentiment-analysis.R
@@ -0,0 +1,2 @@
+# The purpose of this R file is so that GitHub recognizes this project as an R project.
+# GitHub does not recognize .Rmd as an R project, so I'm testing this as a workaround.
